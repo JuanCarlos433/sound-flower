37a42f21525f4f1b77b2638bbc936fa7aa91e074
JuanCarlos433 <95895601+JuanCarlos433@users.noreply.github.com>
2023-11-13 11:47:07 -0600

    funcionalidad de meodo de pago

--- pago.php
+++ pago.php
@@ -1,3 +1,24 @@
+<?php
+    session_start();
+    /*
+    if(!isset($_SESSION['logeado'])|| !$_SESSION['logeado']){
+        header('Location: login.html');
+    }
+    */
+    require_once('assets/php/conexion.php');
+    mysqli_set_charset($conn,'utf8');
+    if($_POST){
+        $usuario=$_SESSION['emailUs'];
+        $sql="UPDATE usuario SET Esta_Suscrito='1' WHERE Correo ='$usuario'";
+        $env=mysqli_query($conn,$sql);
+        echo '<script> alert("Su pago se realizo con exito") </script>';
+        header("Location: pago.php");
+    }
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,8 +29,7 @@
 <body>
     <main class="c2">
         <div  class="logi1">
-           
-            <form action="">
+            <form action="pago.php" method='post'>
                 <h1>Suscribete</h1>
                 <p>Tansolo paga $299.00 y haste vip ya</p>
                 <div class="input-box">
@@ -36,14 +56,14 @@
                     <input type="text" required>
                     <label>Direccion</label>
                 </div>
-                <a href="#" class="c1">
-                    <span id="span1"></span>
+                <div class='c1'>
+                <span id="span1"></span>
+                    <input class='terminar' type='submit' value='paga ya'></input>
                     <span id="span2"></span>
                     <span id="span3"></span>
                     <span id="span4"></span>
-                    Paga ya
-                </a> 
-    
+                </div>
+                
                 <div class="register-link">
                     <p>No tienes dinero? <a href="#">regresar</a></p><br>
                 </div>
